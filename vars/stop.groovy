3e076b438a251dc13e1b6c223aadca8162a24410
MD Monirul Islam <mmonirulislam@fordham.edu>
2019-07-10 16:19:06 -0400

    Update stop.groovy

--- vars/stop.groovy
+++ vars/stop.groovy
@@ -1,37 +1,19 @@
-def call(){
-  node() {
-    
-    stage('try'){
-    
-    
-    cancelStaleBuilds()
-   buildSource()
-
-    
-    
-    }
-
-  }
-    
+def call() {
 def buildSource(){
   for(i = 0; i < 200000; i++)
   {
     print "."
   }
-
+  
 }
-
 def cancelStaleBuilds() {
-
-    
+    stage('Cancel Stale Builds') {
         currentBuildNum = currentBuild.number
         currentBranch = env.BRANCH
-
         try {
             currentBuild.rawBuild.getParent().builds.each {build ->
                 def buildNum = build.number
                 def buildBranch = build.getEnvironment().BRANCH
-
                 if (build.getResult().equals(null) && currentBuildNum > buildNum && currentBranch == buildBranch) {
                     build.doKill()
                     println("[cancelStaleBuilds] Build Cancelled: #${buildNum} ${buildBranch}")
@@ -44,7 +26,8 @@ def cancelStaleBuilds() {
             println("[cancelStaleBuilds] Caught exception: ${e}")
         }
     }
+}
+cancelStaleBuilds()
+buildSource()
 
-
-  
 }

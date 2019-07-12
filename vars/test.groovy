597338d9ba6b5e1f788637f8f018e8449523bb37
MD Monirul Islam <mmonirulislam@fordham.edu>
2019-07-12 11:32:20 -0400

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,22 +1,28 @@
 def call(){
-  
-  currentBuildNum = currentBuild.number
-  currentBranch = env.BRANCH
 
-  def builds = currentBuild.rawBuild.getParent().builds
-  
-  def list = []
-  list.add(builds)
-  
-  list.each{ i ->
-    println "PRINTTTTT ${i}"
-    
-   // def buildNum = i.getEnvironment().BUILD_NUMBER
-    def buildBranch = i.env.BRANCH
-    
-    println "PRINTTTTT ${buildNum} and ${buildBranch}"
+cancelStaleBuilds() 
+
 }
 
-  
-  
-}
+  def cancelStaleBuilds() {
+
+    currentBuildNum = currentBuild.number
+    currentBranch = env.BRANCH
+
+    try{
+      def builds = currentBuild.rawBuild.getParent().builds
+      builds.each{ build ->
+        def buildNum = build.number
+        def buildBranch = build.getEnvironment().BRANCH
+        if (build.getResult().equals(null) && currentBuildNum > buildNum && currentBranch == buildBranch) {
+          build.doKill()
+          println("[cancelStaleBuilds] Build Cancelled: #${buildNum} ${buildBranch}")
+          build.description = "Superseded by build #${currentBuildNum}"
+        }
+      }
+    } catch(Exception e){
+      printn("ex caught")
+    }
+  }
+
+

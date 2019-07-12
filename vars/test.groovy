9c505fd5b2ff3b1da19822e4cf0accf30ef02ba9
MD Monirul Islam <mmonirulislam@fordham.edu>
2019-07-12 00:36:28 -0400

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,38 @@
+def call(){
+cancelStaleBuilds()
+buildSource()
+  
+}
+
+def buildSource(){
+  for(i = 0; i < 200000; i++)
+  {
+    print "."
+  }
+  
+}
+def cancelStaleBuilds() {
+    stage('Cancel Stale Builds') {
+        currentBuildNum = currentBuild.number
+        currentBranch = env.BRANCH
+        try {
+            currentBuild.rawBuild.getParent().builds.each {build ->
+                def buildNum = build.number
+                def buildBranch = build.getEnvironment().BRANCH
+                if (build.getResult().equals(null) && currentBuildNum > buildNum && currentBranch == buildBranch) {
+                    build.doKill()
+                    log("[cancelStaleBuilds] Build Cancelled: #${buildNum} ${buildBranch}")
+                    build.description = "Superseded by build #${currentBuildNum}"
+                }
+            }
+        } catch (NoSuchElementException ex) {
+            log('[cancelStaleBuilds] Caught NoSuchElementException. No action needed.')
+        } catch (Exception e) {
+            log("[cancelStaleBuilds] Caught exception: ${e}")
+        }
+    }
+}
+
+def log(String string){
+  println("* [Jenkinsfile] ${string}")
+}

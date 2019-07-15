d95cb30e1b5494158fd62627d2bfca0b3299bf26
MD Monirul Islam <mmonirulislam@fordham.edu>
2019-07-15 09:08:05 -0400

    Update cancelStaleBuilds.groovy

--- vars/cancelStaleBuilds.groovy
+++ vars/cancelStaleBuilds.groovy
@@ -13,28 +13,30 @@ def buildSource(){
   
 }
 
-def cancelStaleBuilds() {
-  
-  currentBuildNum = currentBuild.number
-  currentBranch = env.BRANCH
-  
-  try{
-    def builds = currentBuild.rawBuild.getParent().builds
-    for(i = 0; i < builds.size(); i++){
-     def buildNum = builds[i].number
-     def buildBranch =  builds[i].getEnvironment().BRANCH
-      
-        if (builds[i].getResult().equals(null) && currentBuildNum > buildNum && currentBranch == buildBranch) {
-        builds[i].doKill()
-        println("[cancelStaleBuilds] Build Cancelled: #${buildNum} ${buildBranch}")
-        builds[i].description = "Superseded by build #${currentBuildNum}"
+  def cancelStaleBuilds() {
+      stage('Cancel Stale Builds') {
+
+          currentBuildNum = currentBuild.number
+          currentBranch = env.BRANCH
+          try {
+              def builds = currentBuild.rawBuild.getParent().builds
+              for (i = 0; i < builds.size(); i++) {
+                  def buildNum = builds[i].number
+                  def buildBranch = builds[i].getEnvironment().BRANCH
+
+                  if (builds[i].isInProgress() && currentBuildNum > buildNum && currentBranch == buildBranch) {
+                      builds[i].doKill()
+                      log("[cancelStaleBuilds] Build Cancelled: #${buildNum} ${buildBranch}")
+                      builds[i].description = "Superseded by build #${currentBuildNum}"
+                  }
+              }
+          } catch (Exception e) {
+              log("[cancelStaleBuilds] Caught exception: ${e}")
+          }
       }
-    
-    }
-    
-  } 
-    catch (Exception e) {
-           println("[cancelStaleBuilds] Caught exception: ${e}")
-        }
-}
+  }
+
+  def log(String string) {
+      println("* [Jenkinsfile] ${string}")
+  }
 
